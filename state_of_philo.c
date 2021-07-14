3f0f91a282a5d27af5b9166436e5c162627bbb8a
ghfdh <dfghdfgh@dfg.dfg>
2021-07-14 21:17:58 +0100

    v4

--- state_of_philo.c
+++ state_of_philo.c
@@ -6,7 +6,7 @@
 /*   By: rel-bour <rel-bour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 20:48:42 by rel-bour          #+#    #+#             */
-/*   Updated: 2021/07/14 21:13:27 by rel-bour         ###   ########.fr       */
+/*   Updated: 2021/07/14 21:17:49 by rel-bour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,9 @@ void *main_philos(void *args)
     printf("%d has taken a fork lift\n", i);
     printf("%d has taken a fork right\n", i);
     printf("%d is eating\n", i);
+    usleep(all->eat_time);
     printf("%d is sleeping\n", i);
+    usleep(all->sleep_time);
     printf("%d is thinking\n", i);
-
-
-    
     return (NULL);
 }
